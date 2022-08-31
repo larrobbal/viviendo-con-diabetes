0d250ea7833792b0068ec3f290eb7adeb8f64180
larrobbal <robb.strike@gmail.com>
2022-08-31 17:21:50 -0500

    Avances

---
+++ assets/mail/contact_me.js
@@ -0,0 +1,217 @@
+$(document).ready(function(){
+    $("#sendMessageButton").click(function (event) {
+        event.preventDefault(); // prevent default submit behaviour
+        // get values from FORM
+        var name = $("input#nombre").val();
+        var email = $("input#correo").val();
+        var phone = $("input#telefono").val();
+        var message = $("textarea#mensaje").val();
+        var firstName = name; // For Success/Failure Message
+        // Check for white space in name for Success/Fail message
+        if (firstName.indexOf(" ") >= 0) {
+            firstName = name.split(" ").slice(0, -1).join(" ");
+        }
+        $this = $("#sendMessageButton");
+        var valid_data = validateData(name,email,phone,message);
+        console.log(valid_data);
+        if(valid_data!==false && typeof valid_data !== undefined)
+        {
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            $.ajax({
+                url: "assets/mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message,
+                },
+                cache: false,
+                success: function () {
+                    // Success message
+                    $("#success").html("<div id='alert-msg' class='alert alert-success bg-green-300 border border-green-500 my-2 mx-5 rounded-lg'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' id='close-alert' class='close inline-flex items-center justify-center text-xl mx-2 my-2' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong class='text-lg'>¡Tu mensaje ha sido enviado con éxito!.</strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                    $('#close-alert').click(function()
+                    {
+                        $('#success').empty();
+                    });
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $("#success").html("<div id='alert-msg' class='alert alert-danger bg-red-300 border border-red-500 my-2 mx-5 rounded-lg'>");
+                    $("#success > .alert-danger")
+                        .html(
+                            "<button type='button' id='close-alert' class='close inline-flex items-center justify-center text-xl mx-2 my-2' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-danger").append(
+                        $("<strong class='text-lg'>").text(
+                            "Lo sentimos " +
+                                firstName +
+                                ", parece ser que el servidor de correo no está respondiendo. ¡Intentalo más tarde!"
+                        )
+                    );
+                    $("#success > .alert-danger").append("</div>");
+                    $('#close-alert').click(function()
+                    {
+                        $('#success').empty();
+                    });
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                },
+            });
+        }
+        function validateData(name,email,phone,message)
+        {
+            var dataArray = {};
+            var characterRegName = /^\s*[a-zA-Z0-9_ ,\s]+\s*$/;
+            var numericRegPhone = /^\s*?[0-9]{10,}\s*$/;
+            var emailRegEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            dataArray['name']=validateName();
+            dataArray['email']=validateMail();
+            dataArray['phone']=validatePhone();
+            dataArray['message']=validateMessage();
+            function validateName()
+            {
+                if(name=="")
+                {
+                    if($("#contactForm #name-contact-div").after)
+                        $("#contactForm #name-contact-div").next('span').remove();
+                    
+                    $("#contactForm #name").addClass('required-field');
+                    $("#contactForm #name").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                    $("#contactForm #name-contact-div").after('<span class="text-red-600" id="alert-message">Por favor, introduce tu nombre.</span>');
+                    return false;
+                }
+                else
+                {
+                    if(!characterRegName.test(name))
+                    {
+                        if($("#contactForm #name-contact-div").after)
+                            $("#contactForm #name-contact-div").next('span').remove();
+
+                        $("input#name").addClass('required-field');
+                        $("input#name").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                        $("#contactForm #name-contact-div").after('<span class="text-red-600">Los caracteres especiales no están permitidos. Por favor, introduce un nombre válido</span>');
+                        return false;
+                    }
+                    else
+                        return true;
+                }
+            }
+            function validateMail()
+            {
+                if(email=="")
+                {
+                    if($("#contactForm #email-contact-div").after)
+                        $("#contactForm #email-contact-div").next('span').remove();
+                    
+                    $("#contactForm #email").addClass('required-field');
+                    $("#contactForm #email").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                    $("#contactForm #email-contact-div").after('<span class="text-red-600" id="alert-message">Por favor, introduce un e-mail.</span>');
+                    return false;
+                }
+                else
+                {
+                    if(!emailRegEmail.test(email))
+                    {
+                        if($("contactForm #email-contact-div").after)
+                            $("contactForm #email-contact-div").next('span').remove();
+
+                        $("input#mail").addClass('required-field');
+                        $("input#mail").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                        $("contactForm #email-contact-div").after('<span class="text-red-600">Formato de E-mail no válido. Por favor, introduce una dirección de correo válida.</span>');
+                        return false;
+                    }
+                    else
+                        return true;
+                }
+            }
+            function validatePhone()
+            {
+                if(phone=="")
+                {
+                    if($("#contactForm #phone-contact-div").after)
+                        $("#contactForm #phone-contact-div").next('span').remove();
+                    
+                    $("#contactForm #phone").addClass('required-field');
+                    $("#contactForm #phone").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                    $("#contactForm #phone-contact-div").after('<span class="text-red-600" id="alert-message">Por favor, introduce un número telefónico.</span>');
+                    return false;
+                }
+                else
+                {
+                    if(!numericRegPhone.test(phone))
+                    {
+                        if($("phone-contact-div").after)
+                            $("phone-contact-div").next('span').remove();
+
+                        $("input#phone").addClass('required-field');
+                        $("input#phone").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                        $("phone-contact-div").after('<span class="text-red-600">Formato de número telefónico no válido. Por favor, introduce un número válido.</span>');
+                        return false;
+                    }
+                    else
+                        return true;
+                }
+            }
+            function validateMessage()
+            {
+                if(message=='')
+                {
+                    if($("#contactForm #message-contact-div").after)
+                        $("#contactForm #message-contact-div").next('span').remove();
+                    
+                    $("#contactForm #message").addClass('required-field');
+                    $("#contactForm #message").css({"background-color":"rgb(255,177,177)","border-color":"#FF0000","border-width":"1px"});
+                    $("#contactForm #message-contact-div").after('<span class="text-red-600" id="alert-message">Por favor, introduce un mensaje.</span>');
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            if(Object.keys(dataArray).every((k) => dataArray[k]))
+                return true;
+            else
+                return false;
+        }
+    });
+    $("#contactForm input").focus(function (event) {
+        if($(this).hasClass('required-field'))
+        {
+            $(this).removeAttr('style');
+            $(this).parent().next('span').remove();
+            $(this).removeClass('required-field');
+        }
+    });
+    $("#contactForm textarea").focus(function (event) {
+        if($(this).hasClass('required-field'))
+        {
+            $(this).removeAttr('style');
+            $(this).parent().next('span').remove();
+            $(this).removeClass('required-field');
+        }
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
---
+++ assets/mail/contact_me.php
@@ -0,0 +1,125 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'ventas@comerzia.com.mx'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Mensaje de contacto enviado por $name";
+$email_body = "<html lang='es'>
+                  <style>
+                     body
+                     {
+                        font-size: 18px; 
+                        font-family: Arial, Helvetica, sans-serif;
+                        text-align: center;
+                     }
+                     span
+                     {
+                        font-weight: bold;
+                     }
+                     img.img-footer
+                     {
+                        width:80%; 
+                        height: auto;
+                     }
+                     span.footer-impresion
+                     {
+                        color: rgb(83, 165, 71);
+                        font-style: italic;
+                        font-size: 12px;
+                     }
+                     table
+                     {
+                        margin-left: 25%;
+                     }
+                  </style>
+                  <head>
+                     <meta charset='utf-8'/>
+                  </head>
+                  <body>
+                     <p>Haz recibido un mensaje desde el formulario de tu web http://www.bolardo.com.mx.</p>
+                     <p style='font-weight: bold;'>Detalles del mensaje:</p>
+                     <span>Nombre:</span> $name<br>
+                     <span>Email:</span> $email_address<br>
+                     <span>Telefono:</span> $phone<br>
+                     <span>Mensaje:</span>
+                     <p>$message<p>
+                     <table>
+                        <tr>
+                              <td><img src='http://www.bolardo.com.mx/assets/img/mail_alt_logo.png' class='img-footer'></td>
+                              <td><span class='footer-impresion'>Antes de imprimir este correo, piense bien si es necesario hacerlo.<br>Si lo hace, utilice papel reciclado. Cuidemos el medio ambiente.</span></td>
+                        </tr>
+                     </table>
+                  </body>
+               </html>";
+$headers = 'From: '.$email_address.''."\r\n".'Reply-To: '.$email_address.''."\r\n".'X-Mailer: PHP/'.phpversion()."\r\n".'Content-type: text/html; charset=UTF-8';  
+mail($to,$email_subject,$email_body,$headers);
+
+$to_client = $email_address;
+$email_subject_client = "Copia de mensaje enviado por $name";
+$email_body_client = "<html lang='es'>
+                        <style>
+                           body
+                           {
+                              font-size: 18px; 
+                              font-family: Arial, Helvetica, sans-serif;
+                              text-align: center;
+                           }
+                           span
+                           {
+                              font-weight: bold;
+                           }
+                           img.img-footer
+                           {
+                              width:80%; 
+                              height: auto;
+                           }
+                           span.footer-impresion
+                           {
+                              color: rgb(83, 165, 71);
+                              font-style: italic;
+                              font-size: 12px;
+                           }
+                           table
+                           {
+                              margin-left: 25%;
+                           }
+                        </style>
+                        <head>
+                           <meta charset='utf-8'/>
+                        </head>
+                        <body>
+                           <p>Haz enviado un mensaje de contacto en la web http://www.bolardo.com.mx.</p>
+                           <p style='font-weight: bold;'>Detalles del mensaje:</p>
+                           <span>Nombre:</span> $name<br>
+                           <span>Email:</span> $email_address<br>
+                           <span>Telefono:</span> $phone<br>
+                           <span>Mensaje:</span>
+                           <p>$message<p>
+                           <p>Gracias por ponerse en contacto con nosotros. En breve recibirá una respuesta.</p>
+                           <table>
+                              <tr>
+                                    <td><img src='http://www.bolardo.com.mx/assets/img/mail_alt_logo.png' class='img-footer'></td>
+                                    <td><span class='footer-impresion'>Antes de imprimir este correo, piense bien si es necesario hacerlo.<br>Si lo hace, utilice papel reciclado. Cuidemos el medio ambiente.</span></td>
+                              </tr>
+                           </table>
+                        </body>
+                     </html>";
+$headers_client = 'From: noreply@comerzia.com.mx'."\r\n".'Reply-To: ventas@comerzia.com.mx'."\r\n".'X-Mailer: PHP/'.phpversion()."\r\n".'Content-type: text/html; charset=UTF-8';
+mail($to_client,$email_subject_client,$email_body_client,$headers_client);
+
+return true;         
+?>
