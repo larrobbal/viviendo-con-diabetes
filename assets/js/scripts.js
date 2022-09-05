2251dd848172d7660751e5a08d2ac3fec5f5945b
larrobbal <robb.strike@gmail.com>
2022-09-05 14:24:15 -0500

    Avances

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -51,3 +51,18 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+var modal = document.getElementById("modal");
+// Get the modal image tag
+var modalImg = document.getElementById("modal-img");
+
+// this function is called when a small image is clicked
+function showModal(src) {
+    modal.classList.remove('hidden');
+    modalImg.src = src;
+}
+
+// this function is called when the close button is clicked
+function closeModal() {
+    modal.classList.add('hidden');
+}
